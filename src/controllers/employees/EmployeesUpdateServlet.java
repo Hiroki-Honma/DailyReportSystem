c72d268c94d83a013849b6bd39da6d96e703fb55
hiroki <hiropon0359@gmail.com>
2021-06-04 11:04:00 +0900

    Add Employee edit

--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -1,11 +1,16 @@
 package controllers.employees;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class EmployeesUpdateServlet
@@ -26,7 +31,12 @@ public class EmployeesUpdateServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	    String _token = request.getParameter("_token");
+	    if(_token != null && _token.equals(request.getSession().getId())){
+	        EntityManager em = DBUtil.createEntityManager();
+	                
+	        Employee e= em.find(Employee.class, (Integer)(request.getSession()))
+	    }
 	}
 
 }
